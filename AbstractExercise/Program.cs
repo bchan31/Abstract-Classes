bb8f977ec735d4ec1c5afe8472f806f1e50e6ec1
bchan31 <59836699+bchan31@users.noreply.github.com>
2020-01-31 09:02:45 -0600

    first commit

---
+++ AbstractExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbstractExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            var motorcycle1 = new Motorcycle();
+
+            car1.Drive();
+            motorcycle1.Drive();
+        }
+    }
+}
---
+++ AbstractExercise/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractExercise
+{
+    abstract class Vehicle
+    {
+        public int Wheels;
+        public int Doors;
+        public abstract void Drive();
+    }
+}
